e9dd36590c7d1eeacc476a235b03b1061de41941
hcslearning <learning@hardcybersoft.cl>
2022-07-04 14:26:33 -0400

    ej: db:seeds

--- codigo/ecommerce/db/seeds.rb
+++ codigo/ecommerce/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Categoria.create([
+    {nombre: "Notebooks"},
+    {nombre: "Teclados"},
+    {nombre: "Mouses"},
+    {nombre: "Monitores"}
+])

2c3be3af7ec5e69c042680d1ba60c62fd8c6f1fe
stgoneira <stgoneiran@gmail.com>
2022-07-04 18:22:38 -0400

    devise config

--- codigo/licitaciones/config/routes.rb
+++ codigo/licitaciones/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :licitantes
+  devise_for :empresas
+  devise_for :administradores
+  root to: "sites#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

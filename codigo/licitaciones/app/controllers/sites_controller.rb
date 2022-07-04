22b3c7a749cd8dbaef66ba1334ae0a6937e67628
stgoneira <stgoneiran@gmail.com>
2022-07-04 19:01:59 -0400

    try form

--- codigo/licitaciones/app/controllers/sites_controller.rb
+++ codigo/licitaciones/app/controllers/sites_controller.rb
@@ -1,5 +1,8 @@
 class SitesController < ApplicationController
     def index 
+    end 
 
+    def search
+        render plain: params[:q]
     end 
 end
--- codigo/licitaciones/config/routes.rb
+++ codigo/licitaciones/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :empresas
   devise_for :administradores
   root to: "sites#index"
+  post "/search", to: "sites#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
